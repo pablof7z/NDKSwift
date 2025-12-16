6390a626598ae3823fe72da60bf89ac3f16e2ad2
pablof7z <pfer@me.com>
2025-12-16 23:16:18 +0200

    Update MarkdownDemo package dependencies to use master branch
    Changed package references from 'pluggable-renderers' to 'master' branch for NDKSwiftCore, NDKSwiftUI, NDKSwiftSQLite, and NDKSwiftNostrDB.

--- Examples/Apps/MarkdownDemo/MarkdownDemoPackage/Package.swift
+++ Examples/Apps/MarkdownDemo/MarkdownDemoPackage/Package.swift
@@ -19,10 +19,10 @@ let package = Package(
         .target(
             name: "MarkdownDemoFeature",
             dependencies: [
-                .product(name: "NDKSwiftCore", package: "pluggable-renderers"),
-                .product(name: "NDKSwiftUI", package: "pluggable-renderers"),
-                .product(name: "NDKSwiftSQLite", package: "pluggable-renderers"),
-                .product(name: "NDKSwiftNostrDB", package: "pluggable-renderers")
+                .product(name: "NDKSwiftCore", package: "master"),
+                .product(name: "NDKSwiftUI", package: "master"),
+                .product(name: "NDKSwiftSQLite", package: "master"),
+                .product(name: "NDKSwiftNostrDB", package: "master")
             ]
         ),
         .testTarget(
