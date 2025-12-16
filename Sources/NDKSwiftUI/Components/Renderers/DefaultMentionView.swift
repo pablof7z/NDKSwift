7205f3b7d987a0da7fc73231c13988dffed73681
pablof7z <pfer@me.com>
2025-12-16 10:42:21 +0200

    feat(renderers): add NDK environment key and DefaultMentionView

---
+++ Sources/NDKSwiftUI/Components/Renderers/DefaultMentionView.swift
@@ -0,0 +1,32 @@
+import SwiftUI
+import NDKSwiftCore
+
+/// Default implementation of MentionRenderer that displays user mentions with profile loading
+public struct DefaultMentionView: MentionRenderer {
+    public let pubkey: String
+    public let npub: String
+    public let onTap: MentionTapHandler?
+
+    @Environment(\.ndk) private var ndk
+    @Environment(\.onMentionTap) private var envOnTap
+
+    public init(pubkey: String, npub: String, onTap: MentionTapHandler? = nil) {
+        self.pubkey = pubkey
+        self.npub = npub
+        self.onTap = onTap
+    }
+
+    public var body: some View {
+        Group {
+            if let ndk = ndk {
+                NDKUIDisplayName(ndk: ndk, pubkey: pubkey)
+            } else {
+                Text("@\(String(npub.prefix(16)))...")
+            }
+        }
+        .foregroundColor(.accentColor)
+        .onTapGesture {
+            (onTap ?? envOnTap)?(pubkey)
+        }
+    }
+}
